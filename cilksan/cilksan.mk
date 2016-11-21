20a82c3ae233df06155ac6c72a632c1737001eb5
TB Schardl <neboat@mit.edu>
2016-11-21 16:00:10 +0000

    [Cilksan] Moving -O3 flag onto a more appropriate Make variable.

--- cilksan/cilksan.mk
+++ cilksan/cilksan.mk
@@ -4,23 +4,23 @@ LTLIBCILKSAN := $(LIB_DIR)/libcilksan.so
 CILKSAN_SRC := cilksan.cpp debug_util.cpp mem_access.cpp driver.cpp print_addr.cpp
 CILKSAN_OBJ := $(CILKSAN_SRC:.cpp=.o)
 
-CILKSAN_CFLAGS = $(TOOL_CFLAGS) -fPIC
+CILKSAN_CFLAGS = $(TOOL_CFLAGS) -fPIC -O3
 CILKSAN_CXXFLAGS = $(CILKSAN_CFLAGS)
 
 LDFLAGS += $(TOOL_LDFLAGS)
 LDLIBS += $(TOOL_LDLIBS)
 
-ifeq ($(COMPILER),LLVM)
-# COMPILER_RT_DIR=$(COMPILER_ROOT)/src/projects/compiler-rt
-# CILKSAN_CFLAGS += -I$(COMPILER_RT_DIR)/lib/ -I$(COMPILER_RT_DIR)/lib/cilk/include
-CILKSAN_CXXFLAGS += $(LIB_CFLAGS) -O3
-# else ifeq ($(COMPILER),GCC)
-# COMPILER_ROOT = $(COMPILERS_HOME)/gcc-cilksan
-# CC = $(COMPILER_ROOT)/bin/gcc
-# CXX = $(COMPILER_ROOT)/bin/g++
-# LIB_CFLAGS += -I $(COMPILER_ROOT)-src/libsanitizer -I$(COMPILER_ROOT)-src/libcilkrts/include 
-# LIB_CXXFLAGS += $(LIB_CFLAGS)
-endif
+# ifeq ($(COMPILER),LLVM)
+# # COMPILER_RT_DIR=$(COMPILER_ROOT)/src/projects/compiler-rt
+# # CILKSAN_CFLAGS += -I$(COMPILER_RT_DIR)/lib/ -I$(COMPILER_RT_DIR)/lib/cilk/include
+# CILKSAN_CXXFLAGS += $(LIB_CFLAGS) -O3
+# # else ifeq ($(COMPILER),GCC)
+# # COMPILER_ROOT = $(COMPILERS_HOME)/gcc-cilksan
+# # CC = $(COMPILER_ROOT)/bin/gcc
+# # CXX = $(COMPILER_ROOT)/bin/g++
+# # LIB_CFLAGS += -I $(COMPILER_ROOT)-src/libsanitizer -I$(COMPILER_ROOT)-src/libcilkrts/include 
+# # LIB_CXXFLAGS += $(LIB_CFLAGS)
+# endif
 
 .PHONY : cleancilksan
 
